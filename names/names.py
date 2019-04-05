4c40520415301bc5b34d51a8097013d42e75caab
Brad Mortensen <b.mort86@gmail.com>
2019-04-05 09:52:05 -0600

    starting names.py

--- names/names.py
+++ names/names.py
@@ -19,3 +19,4 @@ for name_1 in names_1:
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
+# Took 7.8 seconds!
